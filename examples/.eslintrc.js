6ddac05711a7cfaa0b7b907e3a6bd32e5a1b8e2b
Tấn Dự <tandu542003@gmail.com>
2025-10-06 11:31:50 +0700

    revert: completely remove Husky features and examples
    - Remove examples/ directory and all Husky-related files
    - Remove examples/ from package.json files array
    - Remove Examples section and Git Hooks section from README
    - Keep only separate ESLint/Prettier config functionality
    - Version bumped to 1.0.4

--- examples/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-// ESLint configuration example for projects using eslint-prettier-itstandu
-module.exports = {
-  extends: ['eslint-prettier-itstandu'],
-  // You can add project-specific rules here
-  rules: {
-    // Example: Disable a specific rule for your project
-    // '@typescript-eslint/no-explicit-any': 'off',
-  },
-};
